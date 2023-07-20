d654102114f68cd5581192b5545f7705ec374731
N_DK <nguyenkhoadang135759@gmail.com>
2023-07-20 23:13:29 +0700

    Configuration using CSS/SASS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            Lorem ipsum dolor sit amet, consecetur adipiscing elseddo eiusmod
+            tempor.There are many variations of passages of lorem Ipsum
+            available, but the majority have suffered alteration in some
+            injected humour.
         </div>
     );
 }
